0db2447e6a20dc09eef630830fd4d2b09d31bbb8
Nishant Luthra <nishant.luthra30@gmail.com>
2023-04-24 23:36:20 +0530

    changed from cpp to c

---
+++ driver.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("Hello, Mafia CS!");
+
+    return 0;
+}
--- driver.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    
-    cout << "Hello Mafia CS" << endl; 
-    
-    return 0;
-}
